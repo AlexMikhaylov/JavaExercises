737ee0e3e7bd6bfcdf172c3b21a74038cb2a0391
Alexander Mikhaylov <alx.mikhaylov@gmail.com>
2019-07-31 23:33:48 +0200

    added the exercise Perfect Number

---
+++ PerfectNumber/Main.java
@@ -0,0 +1,21 @@
+package PerfectNumber;
+/*
+ * @author Alexander Mikhaylov
+ * @version 1.0
+ * @since 31.07.2019
+ */
+public class Main {
+	/*
+	 * Testing if the number is perfect
+	 */
+	public static void main(String[] args) {
+		System.out.println(PerfectNumber.isPerfectNumber(6));
+		System.out.println(PerfectNumber.isPerfectNumber(28));
+		System.out.println(PerfectNumber.isPerfectNumber(5));
+		System.out.println(PerfectNumber.isPerfectNumber(-1));
+		System.out.println(PerfectNumber.isPerfectNumber(4));
+		
+
+	}
+
+}
