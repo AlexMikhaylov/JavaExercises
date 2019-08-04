5f41a884f29ed3286dc7390cb23674fe93abdbc7
Alexander Mikhaylov <alx.mikhaylov@gmail.com>
2019-08-04 15:12:40 +0200

    added the exercise DiagonalStar

---
+++ DiagonalStar/Main.java
@@ -0,0 +1,17 @@
+package DiagonalStar;
+/*
+ * @author Alexander Mikhaylov
+ * @version 1.0
+ * @since 4.08.2019
+ */
+public class Main {
+	/*
+	 * Testing the method printSquareStar(int number)
+	 */
+	public static void main(String[] args) {
+		DiagonalStar.printSquareStar(5);
+		DiagonalStar.printSquareStar(8);
+
+	}
+
+}
